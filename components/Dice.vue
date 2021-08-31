2d9efcdbde43fc33006c55e0d9c74dbbbbdf0001
Abel Tran <abeltran.develop@gmail.com>
2021-08-31 22:53:27 +0700

    rotate dice 3d

--- components/Dice.vue
+++ components/Dice.vue
@@ -1,13 +1,46 @@
 <template>
-  <div>
-    <model-gltf src="models/dice/scene.gltf" backgroundColor="#FDF3CC"></model-gltf>
-  </div>
+    <model-gltf
+      src="models/dice/scene.gltf"
+      :rotation="rotation"
+      @on-load="onLoad"
+      :backgroundAlpha="0" backgroundColor="#FDF3CC"
+      :scale="{x: 0.7, y: 0.7, z: 0.7}"
+      style="width: 100%; cursor: pointer"
+      @on-click="throwDice"
+    ></model-gltf>
 </template>
 
 <script>
 import { ModelGltf } from 'vue-3d-model'
 
 export default {
+  data() {
+    return {
+      rotation: {
+        x: -Math.PI * Math.random() * 3,
+        y: -Math.PI * Math.random() * 5,
+        z: -Math.PI * Math.random() * 7
+      },
+      isThrowDice: false,
+    }
+  },
+  methods: {
+    onLoad () {
+      this.rotate();
+    },
+    throwDice() {
+      this.isThrowDice = !this.isThrowDice;
+      this.rotate();
+    },
+    rotate () {
+      if (this.isThrowDice) {
+        this.rotation.x += Math.random();
+        this.rotation.y += Math.random();
+        this.rotation.z += Math.random();
+        requestAnimationFrame( this.rotate );
+      }
+    }
+  },
   components: {
     ModelGltf
   }
