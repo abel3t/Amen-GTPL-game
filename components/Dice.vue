e5f164e0c7c28a5001a2548253bac2817f64a6a9
Abel Tran <abeltran.develop@gmail.com>
2021-09-01 20:33:47 +0700

    Update

--- components/Dice.vue
+++ components/Dice.vue
@@ -50,50 +50,52 @@
   justify-items: center;
   padding: 2rem;
   perspective: 600px;
-}
-.die-list {
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: 1fr;
-  height: 6rem;
-  list-style-type: none;
-  transform-style: preserve-3d;
-  width: 6rem;
-}
-.even-roll {
-  transition: transform 1.5s ease-out;
-}
-.odd-roll {
-  transition: transform 1.25s ease-out;
-}
-.die-item {
-  @apply rounded-xl;
-  background-color: #fefefe;
-  box-shadow: inset -0.35rem 0.35rem 0.75rem rgba(0, 0, 0, 0.3),
-  inset 0.5rem -0.25rem 0.5rem rgba(0, 0, 0, 0.15);
-  display: grid;
-  grid-column: 1;
-  grid-row: 1;
-  grid-template-areas:
+  .die-list {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr;
+    height: 6rem;
+    list-style-type: none;
+    transform-style: preserve-3d;
+    width: 6rem;
+    .die-item {
+      @apply rounded-2xl;
+      background-color: #fefefe;
+      box-shadow: inset -0.35rem 0.35rem 0.75rem rgba(0, 0, 0, 0.3),
+      inset 0.5rem -0.25rem 0.5rem rgba(0, 0, 0, 0.15);
+      display: grid;
+      grid-column: 1;
+      grid-row: 1;
+      grid-template-areas:
     "one two three"
     "four five six"
     "seven eight nine";
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(3, 1fr);
-  height: 100%;
-  padding: 1rem;
-  width: 100%;
+      grid-template-columns: repeat(3, 1fr);
+      grid-template-rows: repeat(3, 1fr);
+      height: 100%;
+      padding: 1rem;
+      width: 100%;
+      .dot {
+        align-self: center;
+        background-color: #676767;
+        border-radius: 50%;
+        box-shadow: inset -0.15rem 0.15rem 0.25rem rgba(0, 0, 0, 0.5);
+        display: block;
+        height: 1.25rem;
+        justify-self: center;
+        width: 1.25rem;
+      }
+    }
+  }
 }
-.dot {
-  align-self: center;
-  background-color: #676767;
-  border-radius: 50%;
-  box-shadow: inset -0.15rem 0.15rem 0.25rem rgba(0, 0, 0, 0.5);
-  display: block;
-  height: 1.25rem;
-  justify-self: center;
-  width: 1.25rem;
+
+.even-roll {
+  transition: transform 1s ease-out;
 }
+.odd-roll {
+  transition: transform 1s ease-out;
+}
+
 .even-roll[data-roll="1"] {
   transform: rotateX(360deg) rotateY(720deg) rotateZ(360deg);
 }
@@ -131,22 +133,22 @@
   transform: rotateX(-360deg) rotateY(-900deg) rotateZ(-360deg);
 }
 [data-side="1"] {
-  transform: rotate3d(0, 0, 0, 90deg) translateZ(4rem);
+  transform: rotate3d(0, 0, 0, 90deg) translateZ(3em);
 }
 [data-side="2"] {
-  transform: rotate3d(-1, 0, 0, 90deg) translateZ(4rem);
+  transform: rotate3d(-1, 0, 0, 90deg) translateZ(3em);
 }
 [data-side="3"] {
-  transform: rotate3d(0, 1, 0, 90deg) translateZ(4rem);
+  transform: rotate3d(0, 1, 0, 90deg) translateZ(3em);
 }
 [data-side="4"] {
-  transform: rotate3d(0, -1, 0, 90deg) translateZ(4rem);
+  transform: rotate3d(0, -1, 0, 90deg) translateZ(3em);
 }
 [data-side="5"] {
-  transform: rotate3d(1, 0, 0, 90deg) translateZ(4rem);
+  transform: rotate3d(1, 0, 0, 90deg) translateZ(3em);
 }
 [data-side="6"] {
-  transform: rotate3d(1, 0, 0, 180deg) translateZ(4rem);
+  transform: rotate3d(1, 0, 0, 180deg) translateZ(3em);
 }
 [data-side="1"] .dot:nth-of-type(1) {
   grid-area: five;
@@ -210,26 +212,6 @@
 }
 [data-side="6"] .dot:nth-of-type(6) {
   grid-area: nine;
-}
-
-button {
-  align-self: center;
-  background-color: #efefef;
-  border: none;
-  color: #333;
-  font-size: 1.25rem;
-  font-weight: 700;
-  justify-self: center;
-  padding: 0.5rem 1rem;
-}
-button:hover {
-  cursor: pointer;
-}
-
-@media (min-width: 900px) {
-  .dice {
-    perspective: 1300px;
-  }
 }
 </style>
 
